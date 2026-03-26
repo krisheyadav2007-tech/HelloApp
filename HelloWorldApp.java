66aab89ee339ce06f3fe8a7af2522ff90b03c6ee
Krishe <krisheyadav2007@gmail.com.com>
2026-03-26 13:57:58 +0530

    UC1: Added code to display Hello World

---
+++ HelloWorldApp.java
@@ -0,0 +1,19 @@
+/**
+ * UC1 Display Hello World
+ * 
+ * This class demonstrates a simple Java application that outputs 
+ * the text "Hello World" to the console.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+public class HelloWorldApp {
+
+    // The main method is the application's entry point
+    public static void main(String[] args) {
+        
+        // Output the string literal to the console
+        System.out.println("Hello World");
+        
+    }
+}
